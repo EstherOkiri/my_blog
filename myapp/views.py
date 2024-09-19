1cc73d4f85ab5914ee5165afca93db25133e11f8
EstherOkiri <est.okiri@gmail.com>
2024-09-19 18:51:11 +0300

    cleared name arugument not required

--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html', name='home')
+    return render(request,'index.html')
 
 def about(request):
-    return render(request, 'about.html', name='about')
+    return render(request, 'about.html')
